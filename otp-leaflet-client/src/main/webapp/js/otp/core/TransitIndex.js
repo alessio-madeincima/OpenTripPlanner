d355b4cf2feee813913010ebb46a37813fd180fb
aguiraf <aguiraf@gmail.com>
2014-10-15 10:05:35 +0200

    startTime e endTime erano moltiplicati per 1000 per retrocompatibilità. con questa modifica non più

--- otp-leaflet-client/src/main/webapp/js/otp/core/TransitIndex.js
+++ otp-leaflet-client/src/main/webapp/js/otp/core/TransitIndex.js
@@ -184,8 +184,10 @@ otp.core.TransitIndex = otp.Class({
         var params = {
             agency: agencyId,
             id: stopId,
-            startTime : startTime * 1000, // legacy TransitIndex API still uses milliseconds
-            endTime : endTime * 1000, // legacy TransitIndex API still uses milliseconds
+            /*raf startTime : startTime * 1000, // legacy TransitIndex API still uses milliseconds
+            endTime : endTime * 1000, // legacy TransitIndex API still uses milliseconds */
+            startTime : startTime, 
+            endTime : endTime, 
             extended : true,
         };
         if(otp.config.routerId !== undefined) {
