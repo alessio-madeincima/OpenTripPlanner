02c9b17da0dd8bcf8fcb516630444bbb8d6354e2
aguiraf <aguiraf@gmail.com>
2015-07-30 11:48:18 +0200

    Testi dei box Il servizio e contatti, italiano e inglese

--- src/client/js/otp/locale/Italian.js
+++ src/client/js/otp/locale/Italian.js
@@ -52,13 +52,32 @@ otp.locale.Italian = {
      */
     infoWidgets : [
             {
-                title: 'Progetto',
-                content: '<p>Informazioni sul progetto</p>',
+                title: 'Il Servizio',
+                                content: '<p>Il servizio di Calcolo Percorsi Regionale Pronto TPL è il nuovo strumento di infomobilità che permette di ricercare e pianificare gli spostamenti sul territorio regionale utilizzando i servizi di trasporto pubblico in tutte le sue declinazioni (bus e tram urbani, servizi extraurbani e treno) o tramite percorsi pedonali o con l’utilizzo dell’auto personale.</p>\
+                <br/><br/>\
+                                <p>Il servizio è un progetto sperimentale a cura della <a href="http://www.regione.piemonte.it/trasporti/">Direzione Opere pubbliche, Difesa del suolo, Montagna, Foreste, Protezione civile, Trasporti e Logistica di Regione Piemonte </a> ed è realizzato e gestito da <a href="http://www.5t.torino.it/">5T</a> in collaborazione con le Amministrazioni pubbliche e le aziende di trasporto pubblico piemontesi.</p>\
+                <br/><br/>\
+                                <p>Il Calcolo Percorsi Regionale Pronto TPL utilizza tecnologie open source, largamente diffuse e consolidate come <a href="https://www.openstreetmap.org/">OpenStreetMap</a> e <a href="http://www.opentripplanner.org/">OpenTripPlanner</a> e si basa sugli orari programmati forniti dagli Enti e dalle aziende piemontesi di trasporto pubblico aderenti al <a href="http://bip.piemonte.it/">sistema BIP (Biglietto Integrato Piemonte)</a>.</p>\
+                <br/><br/>\
+                                <p>Per maggiori informazioni su come utilizzare il servizio, si invita a consultare la <a href="http://prontotpl.5t.torino.it/ProntoTPL_Manuale_utente.pdf">Guida all’uso</a>.</p>\
+                ',
                 //cssClass: 'otp-contactWidget',
             },
             {
                 title: 'Contatti',
-                content: '<p>Commenti? Contattaci  su...</p>'
+                content: '<p>Il servizio Calcolo Percorsi Regionale Pronto TPL è un progetto sperimentale e in costante aggiornamento.</p>\
+                                                  <p>Si invita pertanto a segnalare eventuali inesattezze e anomalie e ad inviare consigli e suggerimenti utili al miglioramento del servizio:</p>\
+                                                  <p>-  via email a <a href="mailto:prontotpl@5t.torino.it">prontotpl@5t.torino.it</a></p>\
+                                                  <p>-  contattando il Numero Verde Unico della Regione Piemonte 800333444</p>\
+                                                  <br/><br/>\
+                          <p><b>DISCLAIMER</b></p>\
+                                                  <br/><br/>\
+                                                  <p>Il Calcolo Percorsi Regionale di Pronto TPL utilizza OpenStreetMap (OSM) come base cartografica, un progetto collaborativo finalizzato a creare mappe mondiali a contenuto libero e aggiornato quotidianamente da utenti liberi e indipendenti. Si ricorda pertanto che, sebbene in costante miglioramento, la mappa potrebbe non essere del tutto completa o aggiornata.</p>\
+                                                  <br/><br/>\
+                                                  <p>Si ricorda inoltre che nel riconoscimento degli indirizzi per il punto di partenza e il punto di arrivo potranno verificarsi errori o imprecisioni.</p>\
+                                                  <br/><br/>\
+                                                  <p>Si ricorda infine che i dati inerenti al servizio di trasporto pubblico (quali localizzazione delle fermate, orari e percorsi) sono stati forniti <a href="http://prontotpl.5t.torino.it/ProntoTPL_Aggiornamento_Dati.pdf">dalle Amministrazioni provinciali e comunali della Regione Piemonte e dalle aziende piemontesi di trasporto pubblico</a> e sono generalmente aggiornati con cadenza trimestrale. Nel caso in cui l’aggiornamento non avvenga secondo tali tempistiche, alcune località potrebbero risultare non raggiungibili con i servizi di trasporto pubblico.</p>\
+                 ',
             },
     ],
 
