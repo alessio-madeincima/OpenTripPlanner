becddf4d0ecc5b74cb9496f642b13dd1fae17b96
aguiraf <aguiraf@gmail.com>
2015-10-21 11:21:09 +0200

    Fix time format in link to itinerary

--- src/client/js/otp/modules/planner/PlannerModule.js
+++ src/client/js/otp/modules/planner/PlannerModule.js
@@ -331,12 +331,13 @@ otp.modules.planner.PlannerModule =
        	    queryParams = {
                 fromPlace: this.getStartOTPString(),
                 toPlace: this.getEndOTPString(),
-                time : (this.time) ? otp.util.Time.correctAmPmTimeString(this.time) : moment().format("h:mma"),
+                //time : (this.time) ? otp.util.Time.correctAmPmTimeString(this.time) : moment().format("h:mma"),
                 //time : (this.time) ? moment(this.time).add("s", addToStart).format("h:mma") : moment().add("s", addToStart).format("h:mma"),
                 //date : (this.date) ? moment(this.date, otp.config.locale.time.date_format).format("MM-DD-YYYY") : moment().format("MM-DD-YYYY"),
                 //raf
                 //date : (this.date) ? moment(this.date, otp.config.locale.time.date_format).format(otp.config.locale.time.date_format) : moment().format(otp.config.locale.time.date_format),
                 date : (this.date) ? this.date : moment().format(otp.config.locale.time.date_format),
+                time : (this.time) ? this.time : moment().format(otp.config.locale.time.time_format),
                 mode: this.mode,
                 maxWalkDistance: this.maxWalkDistance
             };
